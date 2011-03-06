73e1a976ab19b822bfee33deef79087b98cb3975
Aemon Cannon <aemoncannon@gmail.com>
2011-03-06 02:29:07 -0800

    Write to temporary files when formatting or expanding in modified buffer.

--- src/main/elisp/ensime.el
+++ src/main/elisp/ensime.el
@@ -54,6 +54,7 @@
 (require 'ensime-refactor)
 (require 'ensime-undo)
 (require 'ensime-search)
+(require 'ensime-doc)
 (eval-when (compile)
   (require 'apropos)
   (require 'compile))
@@ -313,6 +314,7 @@ Do not show 'Writing..' message."
      ["Continue" ensime-db-continue]
      ["Quit" ensime-db-quit]
      ["List local variables" ensime-db-list-locals])
+
     "---"
     ["Go to SBT console" ensime-sbt-switch]
     ["Go to Scala REPL" ensime-inf-switch]
@@ -630,6 +632,19 @@ If not, message the user."
         ((boundp 'temporary-file-directory) temporary-file-directory)
         (t "/tmp/")))
 
+(defun ensime-write-buffer-to-temp-file ()
+  "Write the current buffer to a temp file. Return the full path of the file."
+  (let ((file (ensime-temp-file-name
+	       (file-name-nondirectory
+		(concat ".tmp_" buffer-file-name)))))
+    (ensime-write-buffer file)
+    file))
+
+(defmacro ensime-with-temp-file (name)
+  "Return the path of a temp file with filename 'name'."
+  (concat (file-name-as-directory (ensime-temp-directory))
+          name))
+
 (defun ensime-delete-swank-port-file (&optional quiet)
   (condition-case data
       (delete-file (ensime-swank-port-file))
@@ -723,140 +738,6 @@ The default condition handler for timer functions (see
          (ensime-cancel-connect-retry-timer)
          (message "Cancelled connection attempt."))
         (t (error "Not connecting"))))
-
-
-;; Remote doc lookups
-
-(defcustom ensime-scaladoc-stdlib-url-base
-  "http://www.scala-lang.org/api/current/"
-  "URL base for constructing scaladoc stdlib links."
-  :type 'string
-  :group 'ensime)
-
-(defcustom ensime-scaladoc-compiler-url-base
-  "http://www.scala-lang.org/archives/downloads/distrib/files/nightly/docs/compiler/"
-  "URL for constructing scaladoc compiler links."
-  :type 'string
-  :group 'ensime)
-
-(defcustom ensime-javadoc-stdlib-url-base
-  "http://java.sun.com/javase/6/docs/api/"
-  "URL for constructing javadoc links."
-  :type 'string
-  :group 'ensime)
-
-(defvar ensime-doc-lookup-map
-  '(
-    ("^java\\." . ensime-make-java-doc-url)
-    ("^scala\\.tools\\.nsc\\." . ensime-make-scala-compiler-doc-url)
-    ("^scala\\." . ensime-make-scala-doc-url)
-    ("^android\\." . ensime-make-android-doc-url)
-    )
-  "A map from regular expression strings to functions.
-When ENSIME requests the documentation for a type or member,
-the qualified name of the type will be matched against each
-regex in this map. The corresponding function will be applied
-to the type and or member objects and will return a url string.")
-
-(defun ensime-make-doc-url (type &optional member)
-  "Given a type and an optional member object, yields an http url for
-browsing the documentation for those objects."
-  (catch 'return
-    (dolist (ea ensime-doc-lookup-map)
-      (let ((re (car ea))
-            (func (cdr ea)))
-        (when (integerp (string-match re (ensime-type-full-name type)))
-          (throw 'return (funcall func type member)))))))
-
-(defun ensime-make-android-doc-url (type &optional member)
-  "Given a scala type, and optionally a type member, construct the
-   corresponding scaladoc url."
-  (ensime-make-java-doc-url-helper
-   "http://developer.android.com/reference/" type member))
-
-(defun ensime-make-scala-doc-url (type &optional member)
-  (ensime-make-scala-doc-url-helper
-   ensime-scaladoc-stdlib-url-base type member))
-
-(defun ensime-make-scala-compiler-doc-url (type &optional member)
-  (ensime-make-scala-doc-url-helper
-   ensime-scaladoc-compiler-url-base type member))
-
-(defun ensime-make-scala-doc-url-helper (url-base type &optional member)
-  "Given a scala type, and optionally a type member, construct the
-   corresponding scaladoc url."
-  (let* ((full-type-name (ensime-type-full-name type)))
-    (let* ((s (replace-regexp-in-string "\\." "/" full-type-name)))
-      (concat url-base
-              s
-              ".html"
-              (if member
-                  (let* ((name (ensime-member-name member)))
-                    (concat "#" full-type-name "#" name))))
-      )))
-
-
-(defvar ensime-javadoc-type-replacements
-  '(
-    ("^scala.Any$" . "java.lang.Object")
-    ("^scala.Int$" . "int")
-    ("^scala.Double$" . "double")
-    ("^scala.Short$" . "short")
-    ("^scala.Byte$" . "byte")
-    ("^scala.Long$" . "long")
-    ("^scala.Float$" . "float")
-    ("^scala.Boolean$" . "boolean")
-    ("^scala.Char$" . "char")
-    ("^scala.Unit$" . "void"))
-  "When creating javadoc urls,
-   use this mapping to replace scala types with java types.")
-
-(defun ensime-javadoc-replace-types (str)
-  "Replace scala primitive type names with jave primitive names."
-  (dolist (rep ensime-javadoc-type-replacements)
-    (setq str (replace-regexp-in-string
-               (car rep) (cdr rep) str)))
-  str)
-
-(defun ensime-javadoc-type-name (type)
-  "Return a the java-friendly name for this type."
-  (let ((type-args (ensime-type-type-args type)))
-    (if (and (equal (ensime-type-name type) "Array")
-             type-args)
-        (let ((element-tpe (car type-args)))
-          (concat (ensime-javadoc-replace-types
-                   (ensime-type-full-name element-tpe)) "[]"))
-      (ensime-javadoc-replace-types (ensime-type-full-name type)))))
-
-(defun ensime-make-java-doc-url (type &optional member)
-  (ensime-make-java-doc-url-helper ensime-javadoc-stdlib-url-base type member))
-
-(defun ensime-make-java-doc-url-helper (base-url type &optional member)
-  "Given a java type, and optionally a java type member, construct the
-   corresponding javadoc url."
-  (let* ((full-type-name (ensime-type-full-name type))
-         (without-dollar (replace-regexp-in-string "\\$" "" full-type-name))
-         (with-slashes (replace-regexp-in-string "\\." "/" without-dollar)))
-    (concat base-url
-            with-slashes
-            ".html"
-            (if member
-                (let* ((memb-name (ensime-member-name member))
-
-                       ;; If member is a constructor...
-                       (name (if (equal memb-name "this")
-                                 (ensime-type-name type) memb-name))
-
-                       (type (ensime-member-type member))
-                       (param-types (ensime-type-param-types type)))
-                  (concat
-                   "#" name
-                   "("
-                   (mapconcat
-                    (lambda (tpe)
-                      (ensime-javadoc-type-name tpe)) param-types ", ")
-                   ")"))))
-    ))
 
 
 ;;;; Framework'ey bits
@@ -1152,15 +1033,22 @@ absolute path to f."
 
 
 (defun ensime-revert-visited-files (files &optional typecheck)
+  "files is a list of buffer-file-names to revert or lists of the form
+ (visited-file-name disk-file-name) where buffer visiting visited-file-name
+ will be reverted to the state of disk-file-name."
   (let ((line (ensime-current-line)))
     (save-excursion
       (dolist (f files)
-        (when-let (buf (find-buffer-visiting f))
-          (with-current-buffer buf
-            (let ((before-revert-hook nil)
-                  (after-revert-hook nil))
-              (revert-buffer t t)
-              (ensime-typecheck-current-file))
+	(let* ((dest (cond ((stringp f) f)
+			   ((listp f) (car f))))
+	       (src (cond ((stringp f) f)
+			  ((listp f) (cadr f))))
+	       (do-visit (equal dest src)))
+	  (when-let (buf (find-buffer-visiting dest))
+	    (with-current-buffer buf
+	      (insert-file-contents src do-visit nil nil t)
+	      (when typecheck
+		(ensime-typecheck-current-file)))
             ))))
     (goto-line line)
     ))
@@ -2533,13 +2421,15 @@ any buffer visiting the given file."
   "Format the source in the current buffer using the Scalariform
  formatting library."
   (interactive)
-  (if (buffer-modified-p) (ensime-write-buffer nil t))
-  (message "Formatting...")
-  (ensime-rpc-async-format-files
-   (list buffer-file-name)
-   `(lambda (result)
-      (ensime-revert-visited-files (list ,buffer-file-name) t)
-      )))
+  (let ((file (if (buffer-modified-p)
+		  (ensime-write-buffer-to-temp-file)
+		buffer-file-name)))
+    (message "Formatting...")
+    (ensime-rpc-async-format-files
+     (list file)
+     `(lambda (result)
+	(ensime-revert-visited-files (list (list ,buffer-file-name ,file)) t)
+	))))
 
 ;; Expand selection
 
@@ -2560,7 +2450,6 @@ any buffer visiting the given file."
 (defun ensime-expand-selection-command ()
   "Expand selection to the next widest syntactic context."
   (interactive)
-  (if (buffer-modified-p) (ensime-write-buffer nil t))
   (unwind-protect
       (let* ((continue t)
 	     (ensime-selection-stack (list (list (point) (point))))
@@ -2600,13 +2489,18 @@ any buffer visiting the given file."
 
 (defun ensime-expand-selection (start end)
   "Expand selection to the next widest syntactic context."
-  (let* ((range (ensime-rpc-expand-selection
-		 buffer-file-name start end))
-	 (start (plist-get range :start))
-	 (end (plist-get range :end)))
-    (ensime-set-selection start end)
-    (push (list start end) ensime-selection-stack)
-    ))
+
+  (let ((file (if (buffer-modified-p)
+		  (ensime-write-buffer-to-temp-file)
+		buffer-file-name)))
+
+    (let* ((range (ensime-rpc-expand-selection
+		   file start end))
+	   (start (plist-get range :start))
+	   (end (plist-get range :end)))
+      (ensime-set-selection start end)
+      (push (list start end) ensime-selection-stack)
+      )))
 
 (defun ensime-contract-selection ()
   "Contract to previous syntactic context."
@@ -3011,26 +2905,29 @@ with the current project's dependencies loaded. Returns a property list."
   (let ((ensime-popup-in-other-frame t))
     (ensime-inspect-type-at-point)))
 
+(defun ensime-type-inspect-info-at-point ()
+  "Helper to pull the inspect info for object at point."
+  (let* ((imported-type-path (ensime-imported-type-path-at-point))
+	 (imported-type (when imported-type-path
+			  (ensime-rpc-get-type-by-name-at-point
+			   imported-type-path))))
+    (if imported-type
+	;; if imported type under point
+	(ensime-rpc-inspect-type-by-id
+	 (ensime-type-id imported-type))
+      ;; otherwise do normal type inspection
+      (ensime-rpc-inspect-type-at-point))))
+
 (defun ensime-inspect-type-at-point ()
   "Display a list of all the members of the type under point, sorted by
    owner type."
   (interactive)
   (let ((pack-path (ensime-package-path-at-point)))
-
     ;; inspect package if package under point
     (if pack-path (ensime-inspect-package-by-path pack-path)
 
-      (let* ((imported-type-path (ensime-imported-type-path-at-point))
-	     (imported-type (when imported-type-path
-			      (ensime-rpc-get-type-by-name-at-point
-			       imported-type-path)))
-	     (inspect-info (if imported-type
-			       ;; otherwise, if imported type under point
-			       (ensime-rpc-inspect-type-by-id
-				(ensime-type-id imported-type))
-
-			     ;; otherwise do normal type inspection
-			     (ensime-rpc-inspect-type-at-point))))
+      ;; otherwise, inspect type
+      (let* ((inspect-info (ensime-type-inspect-info-at-point)))
 	(ensime-type-inspector-show inspect-info)))))
 
 (defun ensime-type-inspector-show (info &optional focus-on-member)
